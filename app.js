e9841516933e7a4d49cd0a77c5164cbb55d4ed2a
corbiobio <edarnand@edenschool.fr>
2023-09-18 09:14:58 +0200

    save all project

---
+++ app.js
@@ -0,0 +1,22 @@
+// let value = prompt("choose a age")
+
+// if (value >= 6 && value <= 7) {
+//     console.log('poussin');
+// }else if (value <= 9) {
+//     console.log('pupille');
+// }else if (value <= 11) {
+//     console.log('minime');
+// }else {
+//     console.log('cadet');
+// }
+
+
+// let value1 = prompt("choose a num 1")
+// let value2 = prompt("choose a num 2")
+
+
+// if (value1 > 0 && value2 > 0 || value1 < 0 && value2 < 0 ) {
+//     console.log("positif");
+// }else{
+//     console.log("negatif");
+// }
